2b8650f3721facbc40934b0cc136df5c26d2d8e5
Hồng Lê <thuhongkhanhtoan@gmail.com>
2025-10-09 17:28:46 +0700

    fix bug vnpay

--- ShopOnline/Controllers/PaymentController.cs
+++ ShopOnline/Controllers/PaymentController.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using ShopOnline.Models;
 using System.Web.Mvc;
-using System.Text;
-using System.Security.Cryptography;
 using System.Configuration;
 using System.Linq;
-using System.Net;
-using CSharpVitamins;
 
 namespace ShopOnline.Controllers
 {
@@ -28,133 +23,101 @@ namespace ShopOnline.Controllers
             if (bill == null)
             {
                 return RedirectToAction("MyOrder", "Home", new { memberId = user.memberId });
-            }            
+            }
 
-            string vnp_TmnCode = ConfigurationManager.AppSettings["vnp_TmnCode"];
-            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"];
-            string vnp_Url = ConfigurationManager.AppSettings["vnp_Url"];
-            string vnp_Returnurl = ConfigurationManager.AppSettings["vnp_Returnurl"];
+            //Get Config Info
+            string vnp_Returnurl = ConfigurationManager.AppSettings["vnp_Returnurl"]; //URL nhan ket qua tra ve 
+            string vnp_Url = ConfigurationManager.AppSettings["vnp_Url"]; //URL thanh toan cua VNPAY 
+            string vnp_TmnCode = ConfigurationManager.AppSettings["vnp_TmnCode"]; //Ma định danh merchant kết nối (Terminal Id)
+            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"]; //Secret Key
 
-            // Tạo danh sách tham số gửi đến VNPay
-            Dictionary<string, string> vnp_Params = new Dictionary<string, string>
-        {
-            { "vnp_Version", "2.1.0" },
-            { "vnp_Command", "pay" },
-            { "vnp_TmnCode", vnp_TmnCode },
-            { "vnp_Amount", (bill.totalMoney * 100).ToString() }, // Số tiền tính bằng VND (x100)
-            { "vnp_BankCode", "" },
-            { "vnp_CurrCode", "VND" },
-            { "vnp_TxnRef", invoinceId }, // Mã giao dịch duy nhất
-            { "vnp_OrderInfo", "Thanh toán đơn hàng " + invoinceId },
-            { "vnp_OrderType", "orther" },
-            { "vnp_Locale", "vn" },
-            { "vnp_ReturnUrl", vnp_Returnurl },
-            { "vnp_IpAddr", Request.UserHostAddress },
-            { "vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss") }
-        };
+            //Build URL for VNPAY
+            VnPayLibrary vnpay = new VnPayLibrary();
 
-            // Tạo query string từ tham số
-            string queryString = GenerateQueryString(vnp_Params);
+            vnpay.AddRequestData("vnp_Version", VnPayLibrary.VERSION);
+            vnpay.AddRequestData("vnp_Command", "pay");
+            vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
+            vnpay.AddRequestData("vnp_Amount", (bill.totalMoney * 100).ToString()); // Số tiền tính bằng VND (x100)
+            vnpay.AddRequestData("vnp_BankCode", "");
+            vnpay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss"));
+            vnpay.AddRequestData("vnp_CurrCode", "VND");
+            vnpay.AddRequestData("vnp_IpAddr", Request.UserHostAddress);
+            vnpay.AddRequestData("vnp_Locale", "vn");
+            vnpay.AddRequestData("vnp_OrderInfo", "Thanh toán đơn hàng " + invoinceId);
+            vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
 
-            // Tạo chữ ký
-            string vnp_SecureHash = HmacSHA512(vnp_HashSecret, queryString);
+            vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
+            vnpay.AddRequestData("vnp_TxnRef", invoinceId); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
 
-            // Tạo URL thanh toán
-            vnp_Url += "?" + queryString + "&vnp_SecureHash=" + vnp_SecureHash;
+            //Add Params of 2.1.0 Version
+            vnpay.AddRequestData("vnp_ExpireDate", DateTime.Now.AddMinutes(15).ToString("yyyyMMddHHmmss"));
+            //Billing
+            string paymentUrl = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
 
-            return Redirect(vnp_Url);
+            return Redirect(paymentUrl);
         }
 
         public ActionResult PaymentReturn()
         {
-            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"];
-            var vnp_Params = Request.QueryString.AllKeys.ToDictionary(k => k, k => Request.QueryString[k]);
-
-            // Kiểm tra xem key "vnp_SecureHash" có tồn tại không
-            string vnp_SecureHash = vnp_Params.ContainsKey("vnp_SecureHash") ? vnp_Params["vnp_SecureHash"] : "";
-            vnp_Params.Remove("vnp_SecureHash");
-
-            // Sắp xếp tham số theo thứ tự alphabet
-            vnp_Params = vnp_Params.OrderBy(k => k.Key).ToDictionary(k => k.Key, v => v.Value);
-
-            // Tạo query string từ các tham số
-            string queryString = GenerateQueryString(vnp_Params);
-
-            // Tính toán hash với chuỗi dữ liệu đã tạo
-            string computedHash = HmacSHA512(vnp_HashSecret, queryString);
-
-            // So sánh hash tính toán với hash từ tham số trả về
-            if (computedHash.Equals(vnp_SecureHash, StringComparison.InvariantCultureIgnoreCase))
+            if (Request.QueryString.Count > 0)
             {
-                string invoinceId = vnp_Params["vnp_TxnRef"];
-                ShortGuid gid = (ShortGuid)invoinceId;
-                var item = db.Invoinces.Where(model => model.invoinceId == gid.Guid).FirstOrDefault();
-                if (vnp_Params["vnp_ResponseCode"] == "00")
-                {
-                    item.paymentMethod = "vnpay";
-                    item.paymentStatus = "paid";                    
-                    item.transactionId = vnp_Params["vnp_TransactionNo"];
-                    db.SaveChanges();
+                string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"];
+                var vnpayData = Request.QueryString;
+                VnPayLibrary vnpay = new VnPayLibrary();
 
-                    ViewBag.times = vnp_Params["vnp_PayDate"];
-                    return View(item);
+                foreach (string s in vnpayData)
+                {
+                    if (!string.IsNullOrEmpty(s) && s.StartsWith("vnp_"))
+                    {
+                        vnpay.AddResponseData(s, vnpayData[s]);
+                    }
+                }
+                //vnp_TxnRef: Ma don hang merchant gui VNPAY tai command=pay    
+                //vnp_TransactionNo: Ma GD tai he thong VNPAY
+                //vnp_ResponseCode:Response code from VNPAY: 00: Thanh cong
+                //vnp_SecureHash: HmacSHA512 cua du lieu tra ve
+
+                Guid invoinceId = Guid.Parse(vnpay.GetResponseData("vnp_TxnRef"));
+                 var item = db.Invoinces.Where(model => model.invoinceId == invoinceId).FirstOrDefault();
+
+                string vnp_ResponseCode = vnpay.GetResponseData("vnp_ResponseCode");
+                string vnp_TransactionStatus = vnpay.GetResponseData("vnp_TransactionStatus");
+                String vnp_SecureHash = Request.QueryString["vnp_SecureHash"];
+
+                bool checkSignature = vnpay.ValidateSignature(vnp_SecureHash, vnp_HashSecret);
+                if (checkSignature)
+                {
+                    if (item == null)
+                    {
+                        TempData["msgPaidFailed"] = "Hóa đơn không tồn tại!";
+                        return RedirectToAction("MyOrder", "Home");
+                    }
+
+                    if (vnp_ResponseCode == "00" && vnp_TransactionStatus == "00")
+                    {
+                        item.paymentMethod = "vnpay";
+                        item.paymentStatus = "paid";
+                        item.transactionId = vnpay.GetResponseData("vnp_TransactionNo");
+                        db.SaveChanges();
+
+                        ViewBag.times = vnpay.GetResponseData("vnp_PayDate");
+                        return View(item);
+                    }
+                    else
+                    {
+                        item.paymentStatus = "failed";
+                        db.SaveChanges();
+                        TempData["msgPaidFailed"] = "Thanh toán thất bại!";
+                    }
                 }
                 else
                 {
-                    item.paymentStatus = "failed";
-                    db.SaveChanges();
-                    TempData["msgPaidFailed"] = "Thanh toán thất bại!";
+                    TempData["msgPaidFailed"] = "Có lỗi trong quá trình xử lý.";
                 }
-            }
-            else
-            {
-                TempData["msgPaidFailed"] = "Đã xảy ra lỗi! Vui lòng thử lại";
-            }
+            }      
 
             Member user = (Member)Session["info"];
-            return RedirectToAction("MyOrder", "Home", new { memberId = user.memberId });
-        }
-
-        private string GenerateQueryString(Dictionary<string, string> vnp_Params)
-        {
-            StringBuilder data = new StringBuilder();
-
-            // Sắp xếp tham số theo thứ tự alphabet
-            vnp_Params = vnp_Params.OrderBy(k => k.Key).ToDictionary(k => k.Key, v => v.Value);
-
-            foreach (KeyValuePair<string, string> kv in vnp_Params)
-            {
-                if (!string.IsNullOrEmpty(kv.Value))
-                {
-                    data.Append(WebUtility.UrlEncode(kv.Key) + "=" + WebUtility.UrlEncode(kv.Value) + "&");
-                }
-            }
-
-            // Xóa dấu "&" cuối cùng nếu có
-            string queryString = data.ToString();
-            if (queryString.Length > 0)
-            {
-                queryString = queryString.Remove(queryString.Length - 1, 1);
-            }
-
-            return queryString;
-        }
-
-        public static String HmacSHA512(string key, String inputData)
-        {
-            var hash = new StringBuilder();
-            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-            byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
-            using (var hmac = new HMACSHA512(keyBytes))
-            {
-                byte[] hashValue = hmac.ComputeHash(inputBytes);
-                foreach (var theByte in hashValue)
-                {
-                    hash.Append(theByte.ToString("x2"));
-                }
-            }
-
-            return hash.ToString();
+            return RedirectToAction("MyOrder", "Home", new { memberId = user.memberId});
         }
     }
 }
